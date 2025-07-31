9078528d866f9c9486c5006c5be6197835b5ac83
jenish-vadodariya <202301202@dau.ac.in>
2025-07-31 21:20:50 +0530

    Refactor project structure and implement currency constants
    - Moved App component to src directory
    - Updated import paths in index.js and App.test.tsx
    - Created CurrencyButton component with styles
    - Added currency constants for various currencies
    - Defined currency interface in index.d.ts
    - Added a placeholder

--- src/Components/CurrencyButton.tsx
+++ src/Components/CurrencyButton.tsx
@@ -2,14 +2,34 @@ import { StyleSheet, Text, View } from 'react-native'
 import type { PropsWithChildren } from 'react'
 import React from 'react'
 
+type currecyPropType = PropsWithChildren<{
+    flag: string;
+    name : string;
+}>
 
 
-export default function CurrencyButton() {
-  return (
-    <View>
-      <Text>CurrencyButton</Text>
-    </View>
-  )
+const curreyButton = (properites : currecyPropType):JSX.Element => {
+    return(
+        <View style = {styles.container}>
+            <Text style= {styles.flag}>{properites.flag}</Text>
+            <Text style= {styles.nameT}>{properites.name}</Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center'
+    },
+    flag:{
+        fontSize: 28,
+        color: "#FFFFFF",
+        marginBottom: 4
+    },
+    nameT:{
+        fontSize: 14,
+        color: "#2d3436",
+    }
+})
+
+export default curreyButton
---
+++ src/Components/x.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function x() {
+  return (
+    <View>
+      <Text>x</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
